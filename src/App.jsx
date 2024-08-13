e9ce09b7806a11e846adc400d9d29308e3708686
Ryan <ryanshaunsmith@gmail.com>
2024-08-13 21:03:33 +0100

    create main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import Header from "./components/Header/Header.jsx"
+import Header from "./components/Header/Header.jsx";
+import Main from "./components/Main/Main.jsx";
 
 function App() {
   return (
     <>
         <Header />
-        <main className="site-main grid">
-            <section className="hero grid">
-                <figure>
-                    <img src="images/hero-mobile.png" alt="Cosy, warm fireplace cartoon" />
-                </figure>
-                <div className="hero-text">
-                    <h2>Discover the<br /> Perfect Fireplace...</h2>
-                    <p>
-                        Book consultation: 
-                        <span>0121 345 6789</span>
-                    </p>
-                </div>
-            </section>
-            <section className="how-it-works">
-                <h2>How it works.</h2>
-                <div className="card flex">
-                    <figure className="card-img">
-                        <img src="images/how-it-works-1.png" alt="" />
-                    </figure>
-                    <h3>Give us a call ...</h3>
-                    <p>Call us and book a "Design Consultation" on a date and time to suit you.</p>
-                </div>
-                <div className="card flex">
-                    <figure className="card-img">
-                        <img src="images/how-it-works-2.png" alt="" />                    
-                    </figure>
-                    <h3>We come to you ...</h3>
-                    <p>We come to your home to do an assessment of the space and to your style preference.</p>
-                </div>
-                <div className="card flex">
-                    <figure className="card-img">
-                        <img src="images/how-it-works-3.png" alt="" />
-                    </figure>
-                    <h3>We recommend ...</h3>
-                    <p>We send you a bespoke set of fireplace recommendations.</p>
-                </div>
-            </section>
-        </main>
+        <Main />
         <footer className="site-footer grid">
             <div className="find-us flex">
                 <p>Find us on:</p>
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import styles from "./Main.module.css";
+
+const Main = function() {
+    return (
+        <>
+            <main className={styles.site_main}>
+            <section className="hero grid">
+                <figure>
+                    <img src="images/hero-mobile.png" alt="Cosy, warm fireplace cartoon" />
+                </figure>
+                <div className="hero-text">
+                    <h2>Discover the<br /> Perfect Fireplace...</h2>
+                    <p>
+                        Book consultation: 
+                        <span>0121 345 6789</span>
+                    </p>
+                </div>
+            </section>
+            <section className="how-it-works">
+                <h2>How it works.</h2>
+                <div className="card flex">
+                    <figure className="card-img">
+                        <img src="images/how-it-works-1.png" alt="" />
+                    </figure>
+                    <h3>Give us a call ...</h3>
+                    <p>Call us and book a "Design Consultation" on a date and time to suit you.</p>
+                </div>
+                <div className="card flex">
+                    <figure className="card-img">
+                        <img src="images/how-it-works-2.png" alt="" />                    
+                    </figure>
+                    <h3>We come to you ...</h3>
+                    <p>We come to your home to do an assessment of the space and to your style preference.</p>
+                </div>
+                <div className="card flex">
+                    <figure className="card-img">
+                        <img src="images/how-it-works-3.png" alt="" />
+                    </figure>
+                    <h3>We recommend ...</h3>
+                    <p>We send you a bespoke set of fireplace recommendations.</p>
+                </div>
+            </section>
+        </main>
+        </>
+    );
+}
+
+export default Main;
