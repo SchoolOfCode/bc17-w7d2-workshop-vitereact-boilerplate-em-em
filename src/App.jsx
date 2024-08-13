90a5a56f6bdeffd9ed33755739fba6186f92078f
Ryan <ryanshaunsmith@gmail.com>
2024-08-13 21:12:44 +0100

    created footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import Header from "./components/Header/Header.jsx";
 import Main from "./components/Main/Main.jsx";
+import Footer from './components/Footer/Footer.jsx';
 
 function App() {
   return (
     <>
-        <Header />
-        <Main />
-        <footer className="site-footer grid">
-            <div className="find-us flex">
-                <p>Find us on:</p>
-                <p>
-                    Facebook<br />
-                    Instagram<br />
-                    Tiktok<br />
-                </p>
-            </div>
-            <div className="copyright-info flex">
-                <p>©️ Fireplace Palace</p>
-                <p><a href="mailto:info@firepalace.co.uk">info@firepalace.co.uk</a></p>
-            </div>
-        </footer>
-        </>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   )
 }
 
