2678a134f07a2e28a88e415dcfc49d59fb055625
Ryan <ryanshaunsmith@gmail.com>
2024-08-13 14:03:54 +0100

    tidied up header.jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,8 @@
 
 export default function Header() {
     return (
-        <>
         <header className="site-header">
             <h1>🔥Fireplace Palace</h1>
-            </header>;
-        </>
-            ); 
-            
+            </header>
+    );        
 }
