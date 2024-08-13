58e3f7e5c7cd1a2ed857b16eff1d72c20af3f611
Ryan <ryanshaunsmith@gmail.com>
2024-08-13 19:43:00 +0100

    installed dependencies and copied html and css over into the react app

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,64 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="wrapper grid">
+        <header className="site-header">
+            <h1>🔥Fireplace Palace</h1>
+        </header>
+        <main className="site-main grid">
+            <section className="hero grid">
+                <figure>
+                    <img src="images/hero-mobile.png" alt="Cosy, warm fireplace cartoon" />
+                </figure>
+                <div className="hero-text">
+                    <h2>Discover the<br /> Perfect Fireplace...</h2>
+                    <p>
+                        Book consultation: 
+                        <span>0121 345 6789</span>
+                    </p>
+                </div>
+            </section>
+            <section className="how-it-works">
+                <h2>How it works.</h2>
+                <div className="card flex">
+                    <figure className="card-img">
+                        <img src="images/how-it-works-1.png" alt="" />
+                    </figure>
+                    <h3>Give us a call ...</h3>
+                    <p>Call us and book a "Design Consultation" on a date and time to suit you.</p>
+                </div>
+                <div className="card flex">
+                    <figure className="card-img">
+                        <img src="images/how-it-works-2.png" alt="" />                    
+                    </figure>
+                    <h3>We come to you ...</h3>
+                    <p>We come to your home to do an assessment of the space and to your style preference.</p>
+                </div>
+                <div className="card flex">
+                    <figure className="card-img">
+                        <img src="images/how-it-works-3.png" alt="" />
+                    </figure>
+                    <h3>We recommend ...</h3>
+                    <p>We send you a bespoke set of fireplace recommendations.</p>
+                </div>
+            </section>
+        </main>
+        <footer className="site-footer grid">
+            <div className="find-us flex">
+                <p>Find us on:</p>
+                <p>
+                    Facebook<br />
+                    Instagram<br />
+                    Tiktok<br />
+                </p>
+            </div>
+            <div className="copyright-info flex">
+                <p>©️ Fireplace Palace</p>
+                <p><a href="mailto:info@firepalace.co.uk">info@firepalace.co.uk</a></p>
+            </div>
+        </footer>
+    </div>
   )
 }
 
