b1d0e3ce21aa69c33d5b9b3096a991f7478cbdab
Ryan <ryanshaunsmith@gmail.com>
2024-08-14 08:07:41 +0100

    created HowItWorks component

---
+++ src/components/How_it_works/HowItWorks.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./HowItWorks.module.css";
+
+const HowItWorks = function() {
+    return(
+        <>
+            <section className={styles.how_it_works}>
+                <h2 className={styles.how_it_works_h2}>How it works.</h2>
+                <div className="card flex">
+                    <figure className="card-img">
+                        <img src="images/how-it-works-1.png" alt="" />
+                    </figure>
+                    <h3>Give us a call ...</h3>
+                    <p>Call us and book a "Design Consultation" on a date and time to suit you.</p>
+                </div>
+                <div className="card flex">
+                    <figure className="card-img">
+                        <img src="images/how-it-works-2.png" alt="" />                    
+                    </figure>
+                    <h3>We come to you ...</h3>
+                    <p>We come to your home to do an assessment of the space and to your style preference.</p>
+                </div>
+                <div className="card flex">
+                    <figure className="card-img">
+                        <img src="images/how-it-works-3.png" alt="" />
+                    </figure>
+                    <h3>We recommend ...</h3>
+                    <p>We send you a bespoke set of fireplace recommendations.</p>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default HowItWorks;
